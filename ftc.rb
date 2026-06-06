04c85c25beb33fcfaeda33f72052d71a961d8990
mikhayloboikodev <mikhayloboikodev@gmail.com>
2026-06-06 21:57:35 +0300

    Update ftc.rb

--- ftc.rb
+++ ftc.rb
@@ -1,7 +1,7 @@
 class Ftc < Formula
   desc "This simple CLI tool gives users the opportunity to quickly copy file contents"
   homepage "https://github.com/mikhayloboikodev/CLI-Tool-ftc"
-  url "https://github.com/mikhayloboikodev/CLI-Tool-ftc/archive/refs/tags/cli.tar.gz"
+  url "https://github.com/mikhayloboikodev/CLI-Tool-ftc/archive/refs/tags/cli.tar.gz", using: :nounzip
   version "1.0.0"
   sha256 "e1c59eb7a3a9bfb7c69c25fca8ddce2d1d372e9ef0fbc3da4a9319e1747af0ed"
 
