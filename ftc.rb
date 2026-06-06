1d36fe06e5d32eea15f431fb3f36cc7ba7f54f3a
mikhayloboikodev <mikhayloboikodev@gmail.com>
2026-06-06 22:18:00 +0300

    Update ftc.rb

--- ftc.rb
+++ ftc.rb
@@ -6,6 +6,9 @@ class Ftc < Formula
   sha256 "e1c59eb7a3a9bfb7c69c25fca8ddce2d1d372e9ef0fbc3da4a9319e1747af0ed"
 
   def install
+
+    cd "CLI-Tool-ftc-cli" if buildpath.join("CLI-Tool-ftc-cli").directory?
+    
     system "clang", "main.c", "-o", "ftc"
     bin.install "ftc"
   end
